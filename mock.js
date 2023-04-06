00403ae7390cef5ed1050891cc7d21cc825ff374
Cody Meuer <93593334+stiltlunar@users.noreply.github.com>
2023-04-06 14:37:40 -0500

    add ts

--- mock.js
+++ mock.js
@@ -1,15 +1,11 @@
-const listOne = [];
-const listTwo = [];
-const listThree = [];
-
-const formula = '[listOne] [listTwo] [listThree]';
-
-const sentenceCreator = (formula, ...lists) => {
-  const outputSentences = [];
-  
-  return outputSentences;
-}
-
-const result = sentenceCreator(formula, [listOne, listTwo, listThree]);
-
+var listOne = ['one', 'two'];
+var listTwo = ['three', 'four', 'five'];
+var listThree = ['six', 'seven', 'eight', 'nine'];
+var formula = '[listOne] [listTwo] [listThree]';
+var sentenceCreator = function (formula, lists) {
+    var outputSentences = [];
+    console.log(lists);
+    return outputSentences;
+};
+var result = sentenceCreator(formula, [listOne, listTwo, listThree]);
 console.log(result);
