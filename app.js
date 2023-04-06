821ea1c33bee706a4fec7bf178a6b3eacbe5b7b1
Cody Meuer <93593334+stiltlunar@users.noreply.github.com>
2023-04-06 14:20:29 -0500

    mockup

--- app.js
+++ app.js
@@ -1,5 +1,50 @@
+const fs = require('fs')
 // Read File of Title Formulas and Add to Memory
+const formulas = fs.readFileSync('./formula-test.txt', 'utf-8')
+
+const parsedFormulas = formulas.split('\n')
+
+console.log(parsedFormulas);
 
 // Read File of Keywords and Add to Memory
 
+const text = fs.readFileSync('./test.txt', 'utf-8')
+
+const categories = text.split('\n\n')
+const parsed = []
+
+categories.forEach(category => {
+  const thing = category.split('\n')
+  parsed.push(thing)
+})
+
+console.log(parsed);
+
 // Create Titles and Export to Document
+
+const titles = []
+
+parsedFormulas.forEach(formula => {
+  // find all keyword insertion points
+
+  // forEach number of points, and for each keyword in list, replace keywords in title
+  
+  const splitFormula = formula.split(' ')
+  const placeholders = []
+
+  // Creates a list of all the sentences created by replacing the words
+  splitFormula.forEach(word => {
+    // Find words to replace
+    if (word.slice(0).match(/\[/)) {
+      placeholders.push(word)
+    }
+    // For the number of words to replace, iterate through each word list
+    for (let index = 0; index < placeholders.length; index++) {
+      // Attribute to replace
+      const element = placeholders[index];
+      // Find attribute in list
+      // Replace attribute and move to next one
+    }
+  })
+  console.log(thing.length);
+})
---
+++ mock.js
@@ -0,0 +1,15 @@
+const listOne = [];
+const listTwo = [];
+const listThree = [];
+
+const formula = '[listOne] [listTwo] [listThree]';
+
+const sentenceCreator = (formula, ...lists) => {
+  const outputSentences = [];
+  
+  return outputSentences;
+}
+
+const result = sentenceCreator(formula, [listOne, listTwo, listThree]);
+
+console.log(result);
